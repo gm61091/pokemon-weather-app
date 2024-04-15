d3fd487df9b788fc07cbec552021df4463b49049
gary merriman <g.merriman691@gmail.com>
2024-04-15 12:09:51 -0500

    save

---
+++ server/README.md
@@ -0,0 +1,24 @@
+# pokemon-weather-app
+
+## Overview
+
+The Pokémon Weather App is a web application that utilizes both the Pokémon API and the Weather API to provide users with a list of Pokémon based on the current weather conditions. Users can input a location, and the app will fetch weather information for that location and display Pokémon that are associated with the current weather conditions.
+
+## Features
+
+- **Weather Integration:** Fetches real-time weather information using a weather API based on user-provided location.
+- **Pokémon Listing:** Displays a list of Pokémon types that are associated with the current weather conditions.
+- **Interactive UI:** User-friendly interface for seamless interaction.
+
+## APIs Used
+
+- [Pokémon API](https://pokeapi.co/): Used to retrieve Pokémon information.
+- [Weather API](https://openweathermap.org/api): Used to retrieve weather information.
+
+## Installation
+
+1. Clone the repository: `git clone https://github.com/gm61091/pokemon-weather-app.git`
+2. Navigate to the project directory: `cd pokemon-weather-app`
+3. Install dependencies: `npm install` or `yarn install`
+
+
---
+++ server/server.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const pgp = require('pg-promise')();
+const bcrypt = require('bcrypt');
+const PORT = 3000;
+const saltRounds = 8;
+require('dotenv').config();
+const dbUrl = process.env.DB_KEY; //process is express. react is different
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(cors({origin:'*'})); //star is replaced with url link
+
+// Middleware to parse URL-encoded requests and populate req.body.
+app.use(express.urlencoded({ extended: true }));
+
+
+const db = pgp(dbUrl); //coming from the .env file
+
+
+app.post('/register', async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+        console.log(req.body);
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+        // Store the user data in the database
+         await db.one(`
+            INSERT INTO weahteruser (name, email, password) 
+            VALUES ($1, $2, $3) 
+            RETURNING id`, [name, email, hashedPassword]);
+        console.log('new registered user');
+        res.json('you are registered!');
+
+    } catch (error) {
+        console.error('Error registering user:', error);
+        res.status(500).send('Error registering user');
+    }
+});
+
+app.get('/', async (req, res) => {
+    const allUsers = await  db.many('SELECT * FROM weatheruser');
+    res.send(allUsers);
+});
+
+
+app.post('/login', async (req, res) => {
+    try {
+        // Extract email and password from request body
+        const { email, password } = req.body;
+        
+        // Find the user by email in the database
+        const user = await db.oneOrNone('SELECT * FROM weatheruser WHERE email = $1', email);
+        if (user) {
+            // Compare hashed password with user input
+            const passwordMatch = await bcrypt.compare(password, user.password);
+            if (passwordMatch) {
+                res.send('Login successful');
+            } else {
+                res.status(401).send('Invalid password');
+            }
+        } else {
+            res.status(404).send('User not found');
+        }
+    } catch (error) {
+        console.error('Error authenticating user:', error);
+        res.status(500).send('Error authenticating user');
+    }
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}.`);
+});
+
